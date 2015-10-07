db6c4d01a367fc327a7f0262dc3dbc64d6f23b5b
wellsonjain <wellsonwei@ecoworkinc.com>
2015-10-08 00:32:25 +0800

    Adds shoulda matchers for spec refactors
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

--- Gemfile
+++ Gemfile
@@ -52,4 +52,5 @@ group :test do
   gem "rspec-rails"
   gem "factory_girl_rails"
   gem 'ffaker'
+  gem 'shoulda-matchers'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,20 @@ describe User do
 
   subject { @user }
 
-  it { is_expected.to respond_to(:email) }
-  it { is_expected.to respond_to(:password) }
-  it { is_expected.to respond_to(:password_confirmation) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should alow_value('example@domain.com').for(:email) }
+
+end
+
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_not be_valid }
 end

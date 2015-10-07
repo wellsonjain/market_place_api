2e55618f54bedfac8c521b72019ca296a2acf570
wellsonjain <wellsonwei@ecoworkinc.com>
2015-10-07 18:01:44 +0800

    Add devise user model
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 
 MarketPlaceApi::Application.routes.draw do
+  devise_for :users
   # API defination
   namespace :api, default: {type: :json}, constraints: {subdomain: 'api'}, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do

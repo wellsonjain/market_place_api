ae1c729394342ebf314e7186ca5f63e96bba2e3e
wellsonjain <wellsonwei@ecoworkinc.com>
2015-10-08 08:58:03 +0800

    Adds destroy action to the users controller
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ MarketPlaceApi::Application.routes.draw do
   # API defination
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, :only => [:show, :create, :update]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end

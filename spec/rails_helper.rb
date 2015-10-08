4e01c69f08fc6c7a48fd2e5250746e45d5e072ab
wellsonjain <wellsonwei@ecoworkinc.com>
2015-10-08 11:20:04 +0800

    Refactors the json parse method
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
@@ -50,6 +51,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.include Request::JsonHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|

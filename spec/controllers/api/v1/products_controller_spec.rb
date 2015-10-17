edde705e6882027d4e216970426514b308813816
wellsonjain <wellsonwei@ecoworkinc.com>
2015-10-17 16:01:50 +0800

    Fixes the sessions controller spec
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
     end
 
     it "returns the information about the reporter on a hash" do
-      product_response = json_response
+      product_response = json_response[:product]
       expect(product_response[:title]).to eql @product.title
     end
 
@@ -39,7 +39,7 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       end
 
       it "renders the json representation for the product record just created" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql @product_attributes[:title]
       end
 
@@ -76,7 +76,7 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       end
 
       it "render the json representation for the updated user" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql "An expensive TV"
       end
 
